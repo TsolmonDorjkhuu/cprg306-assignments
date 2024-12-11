834b6f80c5fb8488cb4ca5d5dc5d3b0ffdd38e59
TsolmonDorjkhuu <142355898+TsolmonDorjkhuu@users.noreply.github.com>
2024-12-10 19:00:45 -0700

    Creating assignment
    Starting with student information

---
+++ app/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function StudentInfo(){
+    return (
+        <>
+            <p>Tsolmon Dorjkhuu</p>
+            <Link href={""} style={{textDecoration:"underline"}}>Project Repository</Link>
+        </>
+    )
+}
